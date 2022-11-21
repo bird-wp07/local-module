3587c88c888b1f08458cbbe73d6c4d5f850d9069
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2022-11-21 17:07:35 +0100

    init

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port = process.env.LOCAL_MODULE_PORT ? process.env.LOCAL_MODULE_PORT : 2048;
+
+app.get("/", (_req, res) => {
+    // If run via 'npm st
+    res.send(`Local module v${process.env.npm_package_version}`);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
+});
