5874786f22351dcb8bf877ac381cba626272680e
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2022-11-30 15:48:18 +0100

    Set up logging with winstong
    commit 863eb17738ee6ad6e81b4ad037108e96fc6af94a
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Wed Nov 30 15:45:40 2022 +0100
    
        package-lock.json
    
    commit 2c04fdc87f2b4be1468bc34c12ffa9f38d8baf95
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Wed Nov 30 15:45:25 2022 +0100
    
        set up project-wide logging with winston
    
    commit b76c04336ce8819efadf4cb20cd12e61a953099c
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Wed Nov 30 15:27:43 2022 +0100
    
        set longer timeout for dss wait

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Settings } from "./settings"
+import { logger, Settings } from "./settings"
 import { Dss } from "./dss"
 import express from "express"
 import http from "http"
@@ -14,13 +14,13 @@ async function main() {
     const settings = settingsRes.value
 
     /* Wait for DSS startup to fininsh. */
-    const wait = 60
-    console.log("Waiting for DSS to start up ... ")
+    const wait = 3600
+    logger.info("Waiting for DSS to respond ... ")
     const isOnline = await Dss.isOnline(settings.dssBaseUrl, { waitSeconds: wait })
     if (isOnline) {
-        console.log("DSS responded. Starting HTTP server.")
+        logger.info("DSS responded. Starting HTTP server.")
     } else {
-        console.error(`Could not reach DSS after ${wait} seconds. Abort.`)
+        logger.crit(`Could not reach DSS after ${wait} seconds. Abort.`)
         process.exit(1)
     }
 
@@ -35,7 +35,7 @@ async function main() {
         protocol += "s"
     }
     const initCallback = () => {
-        console.log(`Listening on ${protocol}://${settings.localModuleIp}:${settings.localModulePort}.`)
+        logger.info(`Listening on ${protocol}://${settings.localModuleIp}:${settings.localModulePort}.`)
     }
     if (settings.localModuleUseHttps) {
         // NOTE: This code path is currently inactive.
