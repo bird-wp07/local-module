c8fc8bc0913c78eb8fcd053667572074c1716648
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2022-11-30 18:04:19 +0100

    add todo

--- src/dss/errors.ts
+++ src/dss/errors.ts
@@ -1,3 +1,4 @@
+// FIXME: Create Dss.Error ns and merge with Dss ns while keeping these errors in this file.
 export class NoResponse extends Error {
     public constructor() {
         super("DSS server can't be reached.")
