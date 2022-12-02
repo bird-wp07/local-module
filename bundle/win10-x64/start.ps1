2bcfb3ed4a02a49e782a0d12ba79aaebe8dc5391
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2022-12-02 07:40:06 +0100

    Squashed commit of the following:
    commit f3d4ed33cf5dc87846ff9f7d11730b96ed8e6431
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Fri Dec 2 07:24:12 2022 +0100
    
        prepare a basic readme
    
    commit 7a6182ab175c9e5ecb45b177dbf9a865581286cc
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 19:13:09 2022 +0100
    
        parameterize hiding the dss cmd.exe window
    
    commit b31fd7f1a78e852628350f3220710c5cb437aaf3
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 18:19:37 2022 +0100
    
        disable [x] on pwsh window to avoid skipping cleanup
    
    commit 2ffc32c4041fd2a9d63b8eab1f8a353955b1b868
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 18:07:58 2022 +0100
    
        revert back to original default port
    
    commit b02f78e65647cce0f6274f0a293cfa0c5642193f
    Merge: c314e03 e64c735
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 17:49:00 2022 +0100
    
        Merge branch 'main' into develop
    
    commit c314e03d4e11c142d0a5e7ed525610aafbd63bd4
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 17:34:14 2022 +0100
    
        bump version
    
    commit 27278987a91fb0a32a5586a2582a39e4ae0bb428
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 17:30:32 2022 +0100
    
        fix pipeline
    
    commit 3f5b241206a11a49881592a8c74f0ad2d921d770
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 17:05:30 2022 +0100
    
        fix proken baseurl
    
    commit 079901a524c6fff7dc02f1ea9c7e204b22433f4e
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 16:30:32 2022 +0100
    
        set up server configuration
    
    commit 199e60adc8900e72cba030ef70f707652e9b1d0c
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 16:19:48 2022 +0100
    
        implement in-file substitution of dss port setting
    
    commit e9216004c594339aa1aa975aa772467579a245fa
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 15:15:18 2022 +0100
    
        use dss port instead of baseurl and add comment
    
    commit b9f15927380b05c924789e237bfadb6907833cbf
    Author: Stanislaw Hüll <stanislaw.huell@js-soft.com>
    Date:   Thu Dec 1 14:20:59 2022 +0100
    
        remove private doc from readme

--- bundle/win10-x64/start.ps1
+++ bundle/win10-x64/start.ps1
@@ -2,7 +2,8 @@
 # ADMINISTRATOR SETTINGS
 # ######################
 $LOCAL_MODULE_PORT = 2048
-$DSS_PORT = 8085
+$DSS_PORT = 8080
+$DEBUG = 0
 
 # Constants
 $nodeBinPath = ".\node-v18.12.1-win-x64"
@@ -14,6 +15,35 @@ $serverConfigPath = "$dssRootDir\apache-tomcat-8.5.82\conf\server.xml"
 $rootDir = Split-Path $MyInvocation.MyCommand.Path
 Set-Location $rootDir
 
+# HACK: Disables the pwsh window's [x] button to guarantee that cleanup is
+#       performed. See
+#
+#           https://stackoverflow.com/questions/73746912/disable-the-close-x-button-in-powershell
+function Disable-X {
+    #Calling user32.dll methods for Windows and Menus
+    $MethodsCall = '
+    [DllImport("user32.dll")] public static extern long GetSystemMenu(IntPtr hWnd, bool bRevert);
+    [DllImport("user32.dll")] public static extern bool EnableMenuItem(long hMenuItem, long wIDEnableItem, long wEnable);
+    [DllImport("user32.dll")] public static extern long SetWindowLongPtr(long hWnd, long nIndex, long dwNewLong);
+    [DllImport("user32.dll")] public static extern bool EnableWindow(long hWnd, int bEnable);
+    '
+
+    $SC_CLOSE = 0xF060
+    $MF_DISABLED = 0x00000002L
+
+    #Create a new namespace for the Methods to be able to call them
+    Add-Type -MemberDefinition $MethodsCall -name NativeMethods -namespace Win32
+
+    $PSWindow = Get-Process -Pid $PID
+    $hwnd = $PSWindow.MainWindowHandle
+
+    #Get System menu of windows handled
+    $hMenu = [Win32.NativeMethods]::GetSystemMenu($hwnd, 0)
+
+    #Disable X Button
+    [Win32.NativeMethods]::EnableMenuItem($hMenu, $SC_CLOSE, $MF_DISABLED) | Out-Null
+}
+
 function Start-DSS {
     # HACK: Replace the server's default port by in-file substitution.
     #       Unfortunately, there is no easier method as we're not in control of
@@ -24,9 +54,20 @@ function Start-DSS {
     $content[$lineNr] = $buffer
     Set-Content $serverConfigPath $content
 
-    # Run the included startup batch file from its own directory.
+    # Run the included startup batch file from its own directory. Depdending on
+    # the debug setting, show or hide the dss cmd.exe window.
     Push-Location $dssRootDir
-    Start-Process -FilePath cmd.exe -ArgumentList "/c", ".\Webapp-Startup.bat"
+    $env:JRE_HOME = ".\java"
+    $env:CATALINA_HOME = ".\apache-tomcat-8.5.82"
+    if ($DEBUG -eq 1) {
+        Start-Process -FilePath cmd.exe -ArgumentList "/c", ".\apache-tomcat-8.5.82\bin\catalina.bat run"
+    }
+    else {
+        Start-Process -FilePath cmd.exe -ArgumentList "/c", ".\apache-tomcat-8.5.82\bin\catalina.bat run" `
+            -RedirectStandardError "NUL" -RedirectStandardOut "..\NUL" -NoNewWindow
+            #                       ^^^~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^~~~~~~~~~~~~~~ lol, windows
+            # See https://stackoverflow.com/questions/49375418/start-process-redirect-output-to-null
+    }
     Pop-Location
 }
 
@@ -45,6 +86,8 @@ function Stop-DSS {
 }
 
 function main {
+    Disable-X
+
     # Prepend the nodejs binary abspath to PATH to guarantee that our installed
     # nodejs is used.
     $nodeBin = Resolve-Path $nodeBinPath
@@ -61,6 +104,8 @@ function main {
     }
     finally {
         # Finally ensures cleanup even if the script was ungracefully killed
+        # (e.g. ctrl-c). Finally does not work if we close the windows via the
+        # [x] button (see HACK above).
         Stop-DSS
     }
 }
