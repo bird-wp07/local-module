b4befb876caef92f2d840cc5a0d280111321953f
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2022-12-02 13:20:21 +0100

    implement downloading of dependencies; parse xml via pwsh

--- bundle/win10-x64/start.ps1
+++ bundle/win10-x64/start.ps1
@@ -2,24 +2,41 @@
 # ADMINISTRATOR SETTINGS
 # ######################
 $LOCAL_MODULE_PORT = 2048
-$DSS_PORT = 8080
-$DEBUG = 0
+$DSS_PORT = 8089
+$DEBUG = 1
 
 # Constants
+# ---------
+
+# Path of the standalone node root directory extracted from the archive.
+# Contains, among other files, the 'node.exe' and 'npm.exe' executables.
 $nodeBinPath = ".\node-v18.12.1-win-x64"
+
+# Path of the dss root directory extracted from the archive. Contains, among
+# other files, the 'node.exe' and 'npm.exe' executables.
 $dssRootPath = ".\dss-demo-bundle-5.11.1"
+
+# Path of the local module installed via
+#
+#     npm i --prefix <PREFIX> @bird-wp07/local-module
+#
+# Contains 'package.json', 'package-lock.json' and 'node_modules/.'
+$localModulePath = ".\local-module"
+
+# Path of the apache server's configuration file.
 $serverConfigPath = "$dssRootPath\apache-tomcat-8.5.82\conf\server.xml"
 
-# Change into the directory containing this script for constistent execution
-# irrspective of the working directory from where this script is run.
-$rootDir = Split-Path $MyInvocation.MyCommand.Path
-Set-Location $rootDir
+# Downloads
+$nodeUrl = "https://nodejs.org/dist/v18.12.1/node-v18.12.1-win-x64.zip"
+$nodeZipFileName = ".\node-standalone-v18.12.1-win-x64.zip"
+$dssUrl = "https://ec.europa.eu/digital-building-blocks/artifact/repository/esignaturedss/eu/europa/ec/joinup/sd-dss/dss-demo-bundle/5.11.1/dss-demo-bundle-5.11.1.zip"
+$dssZipFileName = ".\dss-standalone-v5.11.1.zip"
 
 # HACK: Disables the pwsh window's [x] button to guarantee that cleanup is
 #       performed. See
 #
 #           https://stackoverflow.com/questions/73746912/disable-the-close-x-button-in-powershell
-function Disable-X {
+function Set-XButton {
     #Calling user32.dll methods for Windows and Menus
     $MethodsCall = '
     [DllImport("user32.dll")] public static extern long GetSystemMenu(IntPtr hWnd, bool bRevert);
@@ -30,6 +47,7 @@ function Disable-X {
 
     $SC_CLOSE = 0xF060
     $MF_DISABLED = 0x00000002L
+    $MF_ENABLED = 0x00000000L
 
     #Create a new namespace for the Methods to be able to call them
     Add-Type -MemberDefinition $MethodsCall -name NativeMethods -namespace Win32
@@ -40,19 +58,23 @@ function Disable-X {
     #Get System menu of windows handled
     $hMenu = [Win32.NativeMethods]::GetSystemMenu($hwnd, 0)
 
-    #Disable X Button
-    [Win32.NativeMethods]::EnableMenuItem($hMenu, $SC_CLOSE, $MF_DISABLED) | Out-Null
+    if ($args[0] -eq "0") {
+        $state = $MF_DISABLED
+    }
+    else {
+        $state = $MF_ENABLED
+    }
+    [Win32.NativeMethods]::EnableMenuItem($hMenu, $SC_CLOSE, $state) | Out-Null
 }
+
 
 function Start-DSS {
     # HACK: Replace the server's default port by in-file substitution.
     #       Unfortunately, there is no easier method as we're not in control of
     #       the server's configuration.
-    $content = Get-Content $serverConfigPath
-    $lineNr = ($content | Select-String "<!-- ====").LineNumber # pwsh's indexing is off-by-one -_-
-    $buffer = $content[$lineNr] -replace 'port="[^"]*"', "port=`"$DSS_PORT`""
-    $content[$lineNr] = $buffer
-    Set-Content $serverConfigPath $content
+    [xml]$cfg = Get-Content $serverConfigPath
+    $cfg.Server.Service.Connector.port = "$DSS_PORT"
+    $cfg.Save((Resolve-Path $serverConfigPath))
 
     # Run the included startup batch file from its own directory. Depdending on
     # the debug setting, show or hide the dss cmd.exe window.
@@ -66,7 +88,7 @@ function Start-DSS {
         Start-Process -FilePath cmd.exe -ArgumentList "/c", ".\apache-tomcat-8.5.82\bin\catalina.bat run" `
             -RedirectStandardError "NUL" -RedirectStandardOut "..\NUL" -NoNewWindow
         #                           ^^^~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^~~~~~~~~~~~~~~ lol, windows
-        # See https://stackoverflow.com/questions/49375418/start-process-redirect-output-to-null
+        # See https://stackoverflow.com/questions/49375418
     }
     Pop-Location
 }
@@ -85,8 +107,46 @@ function Stop-DSS {
     }
 }
 
+# If needed, downloads missing dependencies (dss, standalone node, local
+# module). Whether a dependency needs to be downloaded is asserted from the
+# existence of the respective directory (dss, local module and node root
+# directory).
+#
+# The contents of a directory, if it exists, are not further inspected. In
+# order to issue a full re-download of a component delete its directory.
+function Install-Dependencies {
+    if (Test-Path -Path $nodeBinPath) {
+        Write-Host "Standalone nodejs distribution found at '$nodeBinPath'."
+    }
+    else {
+        Write-Host "Standalone nodejs distribution not found at '$nodeBinPath'. Starting download ..."
+        Invoke-WebRequest -Uri $nodeUrl -OutFile $nodeZipFileName
+        Write-Host "Extracting archive ..."
+        Expand-Archive -Path $nodeZipFileName -DestinationPath .
+    }
+
+    if (Test-Path -Path $dssRootPath) {
+        Write-Host "DSS installation found at '$dssRootPath'."
+    }
+    else {
+        Write-Host "Dss installation not found at '$dssRootPath'. Starting download ..."
+        Invoke-WebRequest -Uri $dssUrl -OutFile $dssZipFileName
+        Write-Host "Extracting archive ..."
+        Expand-Archive -Path $dssZipFileName -DestinationPath .
+    }
+
+    if (Test-Path -Path $localModulePath) {
+        Write-Host "Local module installation found at '$localModulePath'."
+    }
+    else {
+        Write-Host "Local module installation not found at '$localModulePath'. Starting download ..."
+        # We're using the full path, as the node root hasn't been exported to PATH.
+        Start-Process -FilePath "$nodeBinPath\npm" -ArgumentList "install", "--prefix", $localModulePath, "@bird-wp07/local-module" -Wait
+    }
+}
+
 function main {
-    Disable-X
+    Set-XButton 0
 
     # Prepend the nodejs binary abspath to PATH to guarantee that our installed
     # nodejs is used.
@@ -100,20 +160,28 @@ function main {
         # Start the local module's http server.
         $env:WP07_LOCAL_MODULE_PORT = $LOCAL_MODULE_PORT
         $env:WP07_DSS_BASE_URL = "http://127.0.0.1:$DSS_PORT"
+        Push-Location $localModulePath
         npx "@bird-wp07/local-module"
+        Pop-Location
     }
     finally {
-        # Finally ensures cleanup even if the script was ungracefully killed
-        # (e.g. ctrl-c). Finally does not work if we close the windows via the
-        # [x] button (see HACK above).
+        # Finally ensures cleanup even if the script was killed via ctrl-c.
         Stop-DSS
 
-        # TODO: Enable [x] button after shutdown.
+        # Reactive [x] button and return to original directory.
+        Set-XButton 1
+        Set-Location $rootDir
     }
 }
+
+# Change into the directory containing this script for constistent execution
+# irrspective of the working directory from where this script is run.
+$rootDir = Split-Path $MyInvocation.MyCommand.Path
+Set-Location $rootDir
 
 Switch ($args[0]) {
     stop-dss { Stop-DSS }
     start-dss { Start-DSS }
+    install-dependencies { Install-Dependencies }
     default { main }
 }
