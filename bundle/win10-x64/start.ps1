02df79bdc19d76f73be50b3943d8700201ca8381
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2022-12-02 10:25:07 +0100

    formatting + renaming; add todo

--- bundle/win10-x64/start.ps1
+++ bundle/win10-x64/start.ps1
@@ -7,8 +7,8 @@ $DEBUG = 0
 
 # Constants
 $nodeBinPath = ".\node-v18.12.1-win-x64"
-$dssRootDir = ".\dss-demo-bundle-5.11.1"
-$serverConfigPath = "$dssRootDir\apache-tomcat-8.5.82\conf\server.xml"
+$dssRootPath = ".\dss-demo-bundle-5.11.1"
+$serverConfigPath = "$dssRootPath\apache-tomcat-8.5.82\conf\server.xml"
 
 # Change into the directory containing this script for constistent execution
 # irrspective of the working directory from where this script is run.
@@ -56,7 +56,7 @@ function Start-DSS {
 
     # Run the included startup batch file from its own directory. Depdending on
     # the debug setting, show or hide the dss cmd.exe window.
-    Push-Location $dssRootDir
+    Push-Location $dssRootPath
     $env:JRE_HOME = ".\java"
     $env:CATALINA_HOME = ".\apache-tomcat-8.5.82"
     if ($DEBUG -eq 1) {
@@ -65,8 +65,8 @@ function Start-DSS {
     else {
         Start-Process -FilePath cmd.exe -ArgumentList "/c", ".\apache-tomcat-8.5.82\bin\catalina.bat run" `
             -RedirectStandardError "NUL" -RedirectStandardOut "..\NUL" -NoNewWindow
-            #                       ^^^~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^~~~~~~~~~~~~~~ lol, windows
-            # See https://stackoverflow.com/questions/49375418/start-process-redirect-output-to-null
+        #                           ^^^~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^~~~~~~~~~~~~~~ lol, windows
+        # See https://stackoverflow.com/questions/49375418/start-process-redirect-output-to-null
     }
     Pop-Location
 }
@@ -107,6 +107,8 @@ function main {
         # (e.g. ctrl-c). Finally does not work if we close the windows via the
         # [x] button (see HACK above).
         Stop-DSS
+
+        # TODO: Enable [x] button after shutdown.
     }
 }
 
