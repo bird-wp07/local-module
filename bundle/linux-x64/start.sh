f464f1e73f132bc8014befb794c752028b346d40
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2022-12-14 10:51:43 +0100

    create standalone linux bundle

--- bundle/linux-x64/start.sh
+++ bundle/linux-x64/start.sh
@@ -13,35 +13,37 @@ DSS_PORT=8089
 
 # Path of the standalone node root directory extracted from the archive.
 # Contains, among other files, the 'node.exe' and 'npm' executables.
-nodeBinPath="node-v18.12.1-linux-x64/bin"
+node_root_path="node-v18.12.1-linux-x64"
+node_bin_path="$node_root_path/bin"
 
-nodeUrl="https://nodejs.org/dist/v18.12.1/node-v18.12.1-linux-x64.tar.xz"
-nodeTarFileName="node-standalone-v18.12.1-linux-x64.tar.xz"
+node_url="https://nodejs.org/dist/v18.12.1/node-v18.12.1-linux-x64.tar.xz"
+node_archive_filename="node-standalone-v18.12.1-linux-x64.tar.xz"
 
-javaRootPath="jdk-19.0.1"
-javaUrl="https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.tar.gz"
-javaTarFileName="jdk-19_linux-x64_bin.tar.gz"
+java_root_path="jdk-19.0.1"
+java_url="https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.tar.gz"
+java_archive_filename="jdk-19_linux-x64_bin.tar.gz"
 
-p7zipBinPath="7z"
-p7zipUrl="https://www.7-zip.org/a/7z2201-linux-x64.tar.xz"
-p7zipTarFileName="7z2201-linux-x64.tar.xz"
+p7zip_root_path="7z"
+p7zip_bin_path="$p7zip_root_path"
+p7zip_url="https://www.7-zip.org/a/7z2201-linux-x64.tar.xz"
+p7zip_archive_filename="7z2201-linux-x64.tar.xz"
 
 # Path of the dss root directory extracted from the archive. Contains, among
 # other files, the 'Webapp-Startup.bat' and 'Webapp-Shutdown.bat' batch files.
-dssRootPath="dss-demo-bundle-5.11.1"
+dss_root_path="dss-demo-bundle-5.11.1"
 
-dssUrl="https://ec.europa.eu/digital-building-blocks/artifact/repository/esignaturedss/eu/europa/ec/joinup/sd-dss/dss-demo-bundle/5.11.1/dss-demo-bundle-5.11.1.zip"
-dssZipFileName="dss-standalone-v5.11.1.zip"
+dss_url="https://ec.europa.eu/digital-building-blocks/artifact/repository/esignaturedss/eu/europa/ec/joinup/sd-dss/dss-demo-bundle/5.11.1/dss-demo-bundle-5.11.1.zip"
+dss_archive_filename="dss-standalone-v5.11.1.zip"
 
 # Path of the local module installed via
 #
 #     npm i --prefix <PREFIX> @bird-wp07/local-module
 #
 # Contains 'package.json', 'package-lock.json' and 'node_modules/.'
-localModulePath="./local-module"
+local_module_root_path="./local-module"
 
 # Path of the apache server's configuration file.
-serverConfigPath="$dssRootPath/apache-tomcat-8.5.82/conf/server.xml"
+server_config_path="$dss_root_path/apache-tomcat-8.5.82/conf/server.xml"
 
 # Configure logging. Depending on whether we're running inside a terminal use
 # colors or don't.
@@ -64,7 +66,7 @@ log.err() { __log "err " "\033[31;1m" "$*"; }
 
 install_dependencies() {
     # TODO: Remove dependency on xz for standalone bundle
-    for prog in curl xz; do
+    for prog in curl gzip xz; do
         if ! command -v $prog >/dev/null; then
             log.err "Cannot find required '$prog' binary." >&2
             return 1
@@ -73,66 +75,75 @@ install_dependencies() {
     
     what="${1:-all}"
     if [ "$what" = "7zip" ] || [ "$what" = "all" ]; then
-        if [ -d "$p7zipBinPath" ]; then
-            log.info "7zip binary path found at '$p7zipBinPath'."
+        if [ -d "$p7zip_bin_path" ]; then
+            log.info "7zip binary path found at '$p7zip_bin_path'."
         else
-            log.info "7zip binary path not found at '$p7zipBinPath'. Starting download ..."
-            curl -Lo "$p7zipTarFileName" "$p7zipUrl"
-            mkdir -p "$p7zipBinPath"
-            tar -C "$p7zipBinPath" -xvf "$p7zipTarFileName"
+            log.info "7zip binary path not found at '$p7zip_bin_path'. Starting download ..."
+            curl -Lo "$p7zip_archive_filename" "$p7zip_url"
+            mkdir -p "$p7zip_bin_path"
+            tar -C "$p7zip_bin_path" -xvf "$p7zip_archive_filename"
         fi
     fi
     
     if [ "$what" = "node" ] || [ "$what" = "all" ]; then
-        if [ -d "$nodeBinPath" ]; then
-            log.info "Standalone nodejs distribution found at '$nodeBinPath'."
+        if [ -d "$node_bin_path" ]; then
+            log.info "Standalone nodejs distribution found at '$node_bin_path'."
         else
-            log.info "Standalone nodejs distribution not found at '$nodeBinPath'. Starting download ..."
-            curl -Lo "$nodeTarFileName" "$nodeUrl"
-            tar -xvf "$nodeTarFileName"
+            log.info "Standalone nodejs distribution not found at '$node_bin_path'. Starting download ..."
+            curl -Lo "$node_archive_filename" "$node_url"
+            tar -xvf "$node_archive_filename"
         fi
     fi
     
     if [ "$what" = "dss" ] || [ "$what" = "all" ]; then
-        if [ -d "$dssRootPath" ]; then
-            log.info "DSS installation found at '$dssRootPath'."
+        if [ -d "$dss_root_path" ]; then
+            log.info "DSS installation found at '$dss_root_path'."
         else
-            log.info "DSS installation not found at '$dssRootPath'. Starting download ..."
-            curl -Lo "$dssZipFileName" "$dssUrl"
-            PATH="$p7zipBinPath:$PATH" 7zz x "$dssZipFileName" # the portable 7z's binary is named '7zz', not '7z' (???)
-            chmod +x "$dssRootPath/apache-tomcat-8.5.82/bin/catalina.sh"
+            log.info "DSS installation not found at '$dss_root_path'. Starting download ..."
+            curl -Lo "$dss_archive_filename" "$dss_url"
+            PATH="$p7zip_bin_path:$PATH" 7zz x "$dss_archive_filename" # the portable 7z's binary is named '7zz', not '7z' (???)
+            chmod +x "$dss_root_path/apache-tomcat-8.5.82/bin/catalina.sh"
         fi
     fi
     
     if [ "$what" = "java" ] || [ "$what" = "all" ]; then
-        if [ -d "$javaRootPath" ]; then
-            log.info "Java installation found at '$javaRootPath'."
+        if [ -d "$java_root_path" ]; then
+            log.info "Java installation found at '$java_root_path'."
         else
-            log.info "Java installation not found at '$javaRootPath'. Starting download ..."
-            curl -Lo "$javaTarFileName" "$javaUrl"
-            tar -xvzf "$javaTarFileName"
+            log.info "Java installation not found at '$java_root_path'. Starting download ..."
+            curl -Lo "$java_archive_filename" "$java_url"
+            tar -xvzf "$java_archive_filename"
         fi
     fi
     
     if [ "$what" = "local-module" ] || [ "$what" = "all" ]; then
-        if [ -d "$localModulePath" ]; then
-            log.info "Local module installation found at '$localModulePath'."
+        if [ -d "$local_module_root_path" ]; then
+            log.info "Local module installation found at '$local_module_root_path'."
         else
-            log.info "Local module installation not found at '$localModulePath'. Starting download ..."
-            PATH="$nodeBinPath:$PATH" npm install --prefix "$localModulePath" "@bird-wp07/local-module"
+            log.info "Local module installation not found at '$local_module_root_path'. Starting download ..."
+            PATH="$node_bin_path:$PATH" npm install --prefix "$local_module_root_path" "@bird-wp07/local-module"
         fi
     fi
+}
+
+# Builds self-contained tar.xz archive. Used by pipelines.
+build_standalone_bundle() {
+    archive_filename="${1:-archive.tar.xz}"
+    install_dependencies
+    rm -rf "$dss_root_path/java" # remove embedded java for windows
+    tar -cvJf "$archive_filename" "$java_root_path" "$node_root_path" "$p7zip_root_path" "$local_module_root_path" "README"
+    log.info "Built standalone archive '$archive_filename'."
 }
 
 start_dss() {
     # HACK: Replace the server's default port by in-file substitution.
     #       Unfortunately, there is no easier method as we're not in control of
     #       the server's configuration.
-    sed -i -E 's|(<Connector port=")([^"]+)(" protocol="HTTP/1.1")|\1'"$DSS_PORT"'\3|g' "$serverConfigPath"
+    sed -i -E 's|(<Connector port=")([^"]+)(" protocol="HTTP/1.1")|\1'"$DSS_PORT"'\3|g' "$server_config_path"
     
-    JRE_HOME="$javaRootPath"
-    CATALINA_HOME="$dssRootPath/apache-tomcat-8.5.82"
-    JRE_HOME="$JRE_HOME" CATALINA_HOME="$CATALINA_HOME" "$dssRootPath/apache-tomcat-8.5.82/bin/catalina.sh" run
+    JRE_HOME="$java_root_path"
+    CATALINA_HOME="$dss_root_path/apache-tomcat-8.5.82"
+    JRE_HOME="$JRE_HOME" CATALINA_HOME="$CATALINA_HOME" "$dss_root_path/apache-tomcat-8.5.82/bin/catalina.sh" run
 }
 
 # Lists dss and local-module processes in 'pgrep -af <COMMANDLINE>' syntax.
@@ -159,18 +170,18 @@ stop_dss() {
 
 main() {
     install_dependencies
-    export PATH="$PWD/$nodeBinPath:$PATH"
+    export PATH="$PWD/$node_bin_path:$PATH"
     export WP07_LOCAL_MODULE_PORT=$LOCAL_MODULE_PORT
     export WP07_DSS_BASE_URL="http://127.0.0.1:$DSS_PORT"
     start_dss >/dev/null 2>&1 &
-    cd "$localModulePath"
+    cd "$local_module_root_path"
     npx "@bird-wp07/local-module"
 }
 
 if [ "$1" = "--nocd" ]; then
     shift
 else
-    # Switch to script's directory for consistend execution.
+    # Switch to script's directory for consistent execution.
     cd "$(dirname "$(realpath "$0")")"
 fi
 
