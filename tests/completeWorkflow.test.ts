6138f2112f289183c6975d9ace65949cccd16881
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2022-12-28 14:11:00 +0100

    move const declarations to top of file

--- tests/completeWorkflow.test.ts
+++ tests/completeWorkflow.test.ts
@@ -8,45 +8,6 @@ import { IDigestPDFRequest, IMergePDFRequest, IValidateSignedPdfRequest, IValida
 
 const dssBaseUrl = process.env.DSS_BASEURL ?? "http://127.0.0.1:8080"
 
-describe("Complete workflow", () => {
-    const dssClient = new Dss.DssClient(dssBaseUrl)
-    before("Verify DSS is online", async () => {
-        if ((await dssClient.isOnline()).isErr()) {
-            throw new Error("DSS cannot be reached.")
-        }
-    })
-
-    describe("run poc workflow", () => {
-        test("happy path", async () => {
-            const base64PDF = fs.readFileSync(`./assets/unsigned.pdf`).toString("base64")
-            getDataToSignRequest.base64 = base64PDF
-            const digestResponse = await new service.DigestFacade(dssClient).digestPDF(getDataToSignRequest)
-            const digest = digestResponse.digest
-
-            const getsignedCMSRequest = exampleSignatureRequest
-            getsignedCMSRequest.hash = digest
-            const signature = new centralServiceClient().getSignedCMS(getsignedCMSRequest)
-            if (signature.isErr()) {
-                throw signature.error
-            }
-            const cms = signature.value.cms.toString()
-
-            getSignedDocumentRequest.base64 = base64PDF
-            getSignedDocumentRequest.signatureAsCMS = cms
-            const signedPDF = await new service.MergeFacade(dssClient).mergePDF(getSignedDocumentRequest)
-
-            validateSignedPDFRequest.base64 = signedPDF.base64
-            const validationResult = await new service.ValidateFacade(dssClient).validateSignature(validateSignedPDFRequest)
-            const have = validationResult
-            const want: IValidateSignedPdfResponse = {
-                result: Dss.ESignatureValidationIndication.INDETERMINATE,
-                reason: Dss.ESignatureValidationSubIndication.NO_CERTIFICATE_CHAIN_FOUND
-            }
-            expect(have).to.deep.equal(want)
-        })
-    })
-})
-
 const signatureTimestamp = 1670594222000
 
 const getDataToSignRequest: IDigestPDFRequest = {
@@ -71,3 +32,39 @@ const getSignedDocumentRequest: IMergePDFRequest = {
 const validateSignedPDFRequest: IValidateSignedPdfRequest = {
     base64: "toBeInserted"
 }
+
+describe("Complete workflow", () => {
+    const dssClient = new Dss.DssClient(dssBaseUrl)
+    before("Verify DSS is online", async () => {
+        if ((await dssClient.isOnline()).isErr()) {
+            throw new Error("DSS cannot be reached.")
+        }
+    })
+
+    describe("run poc workflow", () => {
+        test("happy path", async () => {
+            const base64PDF = fs.readFileSync("./assets/unsigned.pdf").toString("base64")
+            getDataToSignRequest.base64 = base64PDF
+            const digestResponse = await new service.DigestFacade(dssClient).digestPDF(getDataToSignRequest)
+            const digest = digestResponse.digest
+
+            const getSignedCMSRequest = { ...exampleSignatureRequest, hash: digest }
+            const signature = new centralServiceClient().getSignedCMS(getSignedCMSRequest)
+            expect(signature.isErr()).to.be.false
+            const cms = signature._unsafeUnwrap().cms.toString()
+
+            getSignedDocumentRequest.base64 = base64PDF
+            getSignedDocumentRequest.signatureAsCMS = cms
+            const signedPDF = await new service.MergeFacade(dssClient).mergePDF(getSignedDocumentRequest)
+
+            validateSignedPDFRequest.base64 = signedPDF.base64
+            const validationResult = await new service.ValidateFacade(dssClient).validateSignature(validateSignedPDFRequest)
+            const have = validationResult
+            const want: IValidateSignedPdfResponse = {
+                result: Dss.ESignatureValidationIndication.INDETERMINATE,
+                reason: Dss.ESignatureValidationSubIndication.NO_CERTIFICATE_CHAIN_FOUND
+            }
+            expect(have).to.deep.equal(want)
+        })
+    })
+})
