18eb2737ff20e32fe17da9a856b0f9003f6e629c
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2023-01-30 13:06:28 +0100

    handle dss shutdown via pidfile
    + remove winapi calls to disable the [x] window buttons as it does not
      work reliably, depending on the terminal emulator used.

--- bundle/win10-x64/start.ps1
+++ bundle/win10-x64/start.ps1
@@ -1,99 +1,65 @@
 # Change into the directory containing this script for constistent execution
 # irrspective of the working directory from where this script is run.
-Set-Location $PSScriptRoot
 
-$LOCAL_MODULE_PORT = Get-Content .\CONFIG | Select-String "^LOCAL_MODULE_PORT=(.*)$" | ForEach-Object{$_.Matches[0].Groups[1].Value}
-$DSS_PORT = Get-Content .\CONFIG | Select-String "^DSS_PORT=(.*)$" | ForEach-Object{$_.Matches[0].Groups[1].Value}
+# scriptDir holds the abspath of the script's or .exe's directory, irrespective
+# of whether we're using a powershell script or an .exe compiled via PS2EXE.
+$scriptDir = if (-not $PSScriptRoot) { Split-Path -Parent (Convert-Path ([environment]::GetCommandLineArgs()[0])) } else { $PSScriptRoot }
+Set-Location $scriptDir
+Write-Host "Starting script at '$scriptDir'."
 
-# Constants
-# ---------
-
-# Path of the standalone node root directory extracted from the archive.
-# Contains, among other files, the 'node.exe' and 'npm' executables.
-$nodeBinPath = ".\node-v18.12.1-win-x64"
-
-# Path of the dss root directory extracted from the archive. Contains, among
-# other files, the 'Webapp-Startup.bat' and 'Webapp-Shutdown.bat' batch files.
-$dssRootPath = ".\dss-demo-bundle-5.11"
-$dssPidFileName = "dss.pid"
-
-# Path of the local module installed via
-#
-#     npm i --prefix <PREFIX> @bird-wp07/local-module
-#
-# Contains 'package.json', 'package-lock.json' and 'node_modules/.'
-$localModulePath = ".\local-module"
-
-# Path of the apache server's configuration file.
-$serverConfigPath = "$dssRootPath\apache-tomcat-8.5.82\conf\server.xml"
-
-# Downloads
+$LOCAL_MODULE_PORT = Get-Content "$scriptDir\CONFIG" | Select-String "^LOCAL_MODULE_PORT=(.*)$" | ForEach-Object{$_.Matches[0].Groups[1].Value}
+$DSS_PORT = Get-Content "$scriptDir\CONFIG" | Select-String "^DSS_PORT=(.*)$" | ForEach-Object{$_.Matches[0].Groups[1].Value}
+$nodeRootPath = "$scriptDir\node-v18.12.1-win-x64"
+$dssRootPath = "$scriptDir\dss-demo-bundle-5.11"
+$dssPidFilePath = "$scriptDir\dss.pid"
+$dssServerConfigPath = "$dssRootPath\apache-tomcat-8.5.82\conf\server.xml" # apache config file
+$localModulePath = "$scriptDir\local-module"
 $nodeUrl = "https://nodejs.org/dist/v18.12.1/node-v18.12.1-win-x64.zip"
-$nodeZipFileName = ".\node-standalone-v18.12.1-win-x64.zip"
 $dssUrl = "https://github.com/bird-wp07/dss-demonstrations/releases/download/5.11/dss-demo-bundle-5.11.zip"
-$dssZipFileName = ".\dss-standalone-v5.11.zip"
 
-# Control the [x] button of the Powershell window, in order to guarantee cleanup. See
-#
-#     https://stackoverflow.com/questions/73746912
-function Set-XButton {
-    #Calling user32.dll methods for Windows and Menus
-    $MethodsCall = '
-    [DllImport("user32.dll")] public static extern long GetSystemMenu(IntPtr hWnd, bool bRevert);
-    [DllImport("user32.dll")] public static extern bool EnableMenuItem(long hMenuItem, long wIDEnableItem, long wEnable);
-    [DllImport("user32.dll")] public static extern long SetWindowLongPtr(long hWnd, long nIndex, long dwNewLong);
-    [DllImport("user32.dll")] public static extern bool EnableWindow(long hWnd, int bEnable);
-    '
-
-    $SC_CLOSE = 0xF060
-    $MF_DISABLED = 0x00000002L
-    $MF_ENABLED = 0x00000000L
-
-    #Create a new namespace for the Methods to be able to call them
-    Add-Type -MemberDefinition $MethodsCall -name NativeMethods -namespace Win32
-
-    $PSWindow = Get-Process -Pid $PID
-    $hwnd = $PSWindow.MainWindowHandle
-
-    #Get System menu of windows handled
-    $hMenu = [Win32.NativeMethods]::GetSystemMenu($hwnd, 0)
-
-    if ($args[0] -eq "0") {
-        $state = $MF_DISABLED
-    }
-    else {
-        $state = $MF_ENABLED
-    }
-    [Win32.NativeMethods]::EnableMenuItem($hMenu, $SC_CLOSE, $state) | Out-Null
+function Stop-ProcessTree
+{
+    Param(
+        [Parameter(Mandatory=$True,Position=1)]
+            [int]$parentProcessId
+    )
+    Get-WmiObject win32_process | where {$_.ParentProcessId -eq $parentProcessId} | ForEach { Stop-ProcessTree $_.ProcessId }
+    Get-WmiObject win32_process | where {$_.ParentProcessId -eq $parentProcessId} | ForEach { Stop-Process $_.ProcessId 2>$null }
 }
 
 function Start-DSS {
     # HACK: Replace the server's default port by in-file substitution.
     #       Unfortunately, there is no easier method as we're not in control of
     #       the server's configuration.
-    [xml]$cfg = Get-Content $serverConfigPath
+    [xml]$cfg = Get-Content $dssServerConfigPath
     $cfg.Server.Service.Connector.port = "$DSS_PORT"
-    $cfg.Save((Resolve-Path $serverConfigPath)) # Writeback requires abspath
+    $cfg.Save($dssServerConfigPath) # NOTE: writeback requires abspath
 
-    # Run the included startup batch file from its own directory.
-    Push-Location $dssRootPath
-    $env:JRE_HOME = ".\java"
-    $env:CATALINA_HOME = ".\apache-tomcat-8.5.82"
-    $process = Start-Process -FilePath cmd.exe -ArgumentList "/c", ".\apache-tomcat-8.5.82\bin\catalina.bat run" `
-        -RedirectStandardError "NUL" -RedirectStandardOut "..\NUL" -NoNewWindow
-    #                           ^^^~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^~~~~~~~~~~~~~~ lol, windows
-    # See https://stackoverflow.com/questions/49375418
-    Write-Output $process.ID >dss.pid
-    Pop-Location
+    # Start DSS in its proper environment and write the (parent cmd.exe's) process id to file.
+    $env:JAVA_HOME = "$dssRootPath\java"
+    $env:JRE_HOME = $env:JAVA_HOME
+    $env:Path = "$env:JAVA_HOME\bin;$env:Path"
+    $env:CATALINA_HOME = "$dssRootPath\apache-tomcat-8.5.82"
+    Start-Process -FilePath cmd.exe `
+        -ArgumentList "/c", "$dssRootPath\apache-tomcat-8.5.82\bin\catalina.bat run" `
+        -RedirectStandardError "NUL" -RedirectStandardOut "..\NUL" -NoNewWindow `
+        -PassThru |
+        Select-Object -ExpandProperty Id |
+        Out-File -FilePath "$dssPidFilePath"
 }
 
-# Kill running DSS servers via PID. This is a hack to solve the problem of the
-# included shutdown batch file not working properly unless the dss server has
-# fully finished its boot sequence.
 function Stop-DSS {
-    Push-Location $dssRootPath
-    $dssPid = Get-Content -File dss.pid
-    Pop-Location
+    if (Test-Path -Path $dssPidFilePath) {
+        $dssPid = Get-Content -Path $dssPidFilePath -First 1
+        # NOTE: We kill the process and its children. For unknown reasons
+        #       just killing the cmd.exe parent process worked for the
+        #       regular user, but failed if the scripts was run in an
+        #       administrator powershell.
+        Stop-ProcessTree $dssPid
+        Remove-Item $dssPidFilePath
+    } else {
+        Write-Host "Stop-Dss: Pidfile '$dssPidFilePath' not found."
+    }
 }
 
 # If needed, downloads missing dependencies (dss, standalone node, local
@@ -105,35 +71,35 @@ function Stop-DSS {
 # order to issue a full re-download of a component delete its directory.
 function Install-Dependencies ($what, $lmver = "latest") {
     if (($what -eq "node") -or ($what -eq "all")) {
-        if (Test-Path -Path $nodeBinPath) {
-            Write-Output "Standalone nodejs distribution found at '$nodeBinPath'."
+        if (Test-Path -Path $nodeRootPath) {
+            Write-Host "Standalone nodejs distribution found at '$nodeRootPath'."
         }
         else {
-            Write-Output "Standalone nodejs distribution not found at '$nodeBinPath'. Starting download ..."
-            Invoke-WebRequest -Uri $nodeUrl -OutFile $nodeZipFileName
-            Write-Output "Extracting archive ..."
-            Expand-Archive -Path $nodeZipFileName -DestinationPath .
+            Write-Host "Standalone nodejs distribution not found at '$nodeRootPath'. Starting download ..."
+            Invoke-WebRequest -Uri $nodeUrl -OutFile "$scriptDir\node.zip"
+            Write-Host "Extracting archive ..."
+            Expand-Archive -Path "$scriptDir\node.zip" -DestinationPath $scriptDir
         }
     }
 
     if (($what -eq "dss") -or ($what -eq "all")) {
         if (Test-Path -Path $dssRootPath) {
-            Write-Output "DSS installation found at '$dssRootPath'."
+            Write-Host "DSS installation found at '$dssRootPath'."
         }
         else {
-            Write-Output "Dss installation not found at '$dssRootPath'. Starting download ..."
-            Invoke-WebRequest -Uri $dssUrl -OutFile $dssZipFileName
-            Write-Output "Extracting archive ..."
-            Expand-Archive -Path $dssZipFileName -DestinationPath .
+            Write-Host "Dss installation not found at '$dssRootPath'. Starting download ..."
+            Invoke-WebRequest -Uri $dssUrl -OutFile "$scriptDir\dss.zip"
+            Write-Host "Extracting archive ..."
+            Expand-Archive -Path "$scriptDir/dss.zip" -DestinationPath $scriptDir
         }
     }
 
     if (($what -eq "local-module") -or ($what -eq "all")) {
         if (Test-Path -Path $localModulePath) {
-            Write-Output "Local module installation found at '$localModulePath'."
+            Write-Host "Local module installation found at '$localModulePath'."
         }
         else {
-            Write-Output "Local module installation not found at '$localModulePath'. Starting download ..."
+            Write-Host "Local module installation not found at '$localModulePath'. Starting download ..."
             if ($lmver -eq "latest") {
                 $suffix = "latest"
             } else {
@@ -144,7 +110,7 @@ function Install-Dependencies ($what, $lmver = "latest") {
                 Select-Object -Expand Content |
                 ConvertFrom-Json |
                 Select-Object -Expand zipball_url
-            Invoke-WebRequest -Uri $zipballUrl -OutFile lm.zip
+            Invoke-WebRequest -Uri $zipballUrl -OutFile "$scriptDir\lm.zip"
             Expand-Archive -Path lm.zip -DestinationPath $localModulePath
 
             # Remove subdirectory in archive (equivalent of tar --strip-components 1).
@@ -154,10 +120,7 @@ function Install-Dependencies ($what, $lmver = "latest") {
             Copy-Item -Recurse "$localModulePath\$subdirName\*" -Destination "$localModulePath"
             Remove-Item -Recurse "$localModulePath\$subdirName"
 
-            # Prepend the nodejs binary abspath to PATH to guarantee that our installed
-            # nodejs is used.
-            $nodeBin = Resolve-Path $nodeBinPath
-            $env:Path = "$($nodeBin);$($env:Path)"
+            $env:Path = "$nodeRootPath;$env:Path"
             Push-Location $localModulePath
             npm install
             npm run build-windows
@@ -167,33 +130,25 @@ function Install-Dependencies ($what, $lmver = "latest") {
 }
 
 function main {
-    $version = $(Get-Content -Path VERSION)
+    $version = "latest"
+    if (Test-Path -Path "$scriptDir\VERSION") {
+        $version = Get-Content -Path "$scriptDir\VERSION"
+    }
     Install-Dependencies all $version
 
-    # Prepend the nodejs binary abspath to PATH to guarantee that our installed
-    # nodejs is used.
-    $nodeBin = Resolve-Path $nodeBinPath
-    $env:Path = "$($nodeBin);$($env:Path)"
-
     try {
-        # Start DSS in the background in a separate cmd.exe window.
-        Set-XButton 0
         Start-DSS
 
-        # Start the local module's http server.
         $env:WP07_LOCAL_MODULE_BASEURL = "http://127.0.0.1:$LOCAL_MODULE_PORT"
         $env:WP07_DSS_BASEURL = "http://127.0.0.1:$DSS_PORT"
+        $env:Path = "$nodeRootPath;$env:Path"
         Push-Location $localModulePath
         npm run start-windows
         Pop-Location
     }
     finally {
-        # Finally ensures cleanup even if the script was killed via ctrl-c.
         Stop-DSS
-
-        # Reactive [x] button and return to original directory.
-        Set-XButton 1
-        Set-Location $rootDir
+        Set-Location $scriptDir
     }
 }
 
